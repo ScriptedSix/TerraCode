59060ea0f8c0071fb519d990e783f5738a16eb55
Семенова Елизавета <thealeeeaaa23@gmail.com>
2025-11-14 03:08:13 -0500

    created a login page

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import Signup from './user/Signup.jsx'
+import Signin from './user/Signin.jsx'
+
+// import "./styles/styles.css";
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+
+// import ScrollToTop from "./utils/scrollToTop"
+
+function App() {
+  return (
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route path="/signup" element={<Signup />}/>
+					<Route path="/signin" element={<Signin />} />
+				</Routes>
+			</Router>
+		</div>
+  );
+}
+
+export default App;
